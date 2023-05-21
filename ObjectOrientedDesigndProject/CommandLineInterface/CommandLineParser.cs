43910e8601602f6fd99b76f1511b91ef4bf4f978
Witold Trzeciakowski <w.trzeci@gmail.com>
2023-05-21 17:00:07 +0200

    added editing entries

--- ObjectOrientedDesigndProject/CommandLineInterface/CommandLineParser.cs
+++ ObjectOrientedDesigndProject/CommandLineInterface/CommandLineParser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ObjectOrientedDesigndProject.classes_Base;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,6 +12,7 @@ namespace ObjectOrientedDesigndProject
 {
     internal static class CommandLineUtility
     {
+        static Queue<String> messageQueue = new Queue<string>(); 
     
 
         public static void GetCommands(Bitflix bitflix)
@@ -19,6 +21,28 @@ namespace ObjectOrientedDesigndProject
             while(true)
             {
                 var line = Console.ReadLine();
+                if (line == "queue commit")
+                    ProcessCommands(bitflix);
+                else if (line == "EXIT")
+                    break;
+                else if (line == "queue print")
+                    PrintQueue();
+                else
+                    addMessagesToTheQueue(line);
+                Console.WriteLine("Awaiting next command");
+            }
+        }
+        private static void PrintQueue()
+        {
+            foreach(var command in messageQueue)
+            {
+                Console.WriteLine(command);
+            }
+        }
+        private static void ProcessCommands(Bitflix bitflix)
+        {
+            foreach (var line in messageQueue)
+            {
                 string[] parsed = line.Split(' ');
                 string command = parsed[0];
                 string[] args = parsed.Skip(1).ToArray();
@@ -26,12 +50,12 @@ namespace ObjectOrientedDesigndProject
                 {
                     case "list":
                         Console.WriteLine("PRINTING LIST:");
-                        if(args.Length != 1) { throw new Exception("wrong numbner of args!"); }
-                        PrintListOfType(args[0],bitflix);
+                       
+                        PrintListOfType(args[0], bitflix);
                         break;
                     case "find":
                         Console.WriteLine("FINDING");
-                        if ((args.Length-1) %3 != 0) { throw new Exception("List of Args has to be divisible by 3"); }
+                        if ((args.Length - 1) % 3 != 0) { throw new Exception("List of Args has to be divisible by 3"); }
                         PrintListWithRequaierments(args, bitflix);
                         break;
                     case "EXIT":
@@ -40,13 +64,23 @@ namespace ObjectOrientedDesigndProject
                     case "add":
                         Console.WriteLine("ADDING OBJECT");
                         AddObjectToList(args, bitflix);
-
+                        break;
+                    case "edit":
+                        Console.WriteLine("EDITING OBJECT");
+                        UpdateRecordsWithGivenRequierment(bitflix, args);
                         break;
                     default:
-                        throw new Exception("UNRECOGNIZED COMMAND");
+                        Console.WriteLine(line + " unrecognized command");
+                        break;
                 }
-                Console.WriteLine("Awaiting next command");
+
             }
+            messageQueue.Clear();
+
+        }
+        private static void addMessagesToTheQueue(string line)
+        {
+            messageQueue.Enqueue(line);
         }
         private static void PrintListOfType(string objectName, Bitflix bitflix)
         {
@@ -78,6 +112,81 @@ namespace ObjectOrientedDesigndProject
                     }
                 }
             }
+        }
+        private static void UpdateRecordsWithGivenRequierment(Bitflix bitflix,string[] args)
+        {
+
+            dynamic obj = bitflix.GetTableOfName(args[0]);
+            Func<object, object, bool> func = bitflix.GetFuncOfName(args[2]);
+
+            for (int i=0; i < obj.Count; i++)
+            {
+                object tmp = obj[i];
+                InterfaceBasse temp = tmp as InterfaceBasse;
+                if (temp.Properties()[args[1]] is int)
+                {
+                    if (func(temp.Properties()[args[1]], int.Parse(args[3])))
+                    {
+                        tmp= UpdateObject(ref temp, bitflix);
+                    }
+                }
+                else if (temp.Properties()[args[1]] is string)
+                {
+                    if (func(temp.Properties()[args[1]], args[3]))
+                    {
+                        tmp= UpdateObject(ref temp, bitflix);
+                    }
+                }
+                
+                obj[i] = tmp as dynamic;
+            }
+
+        }
+        private static object UpdateObject(ref InterfaceBasse temp, Bitflix bitflix)
+        {
+            int i = 0;
+            var values = new List<string>();
+            foreach (var temp2 in temp.Properties().Values)
+            {
+                if (temp2 is string)
+                     values.Add((string)temp2);
+                else
+                {
+                    values.Add(temp2.ToString());
+                }
+
+            }
+
+        for (; ; )
+            {
+                foreach (string temp2 in temp.Properties().Keys)
+                {
+                    Console.Write(temp2 + ": ");
+                    string value = Console.ReadLine();
+
+                    if (value == "EXIT")
+                    {
+                        return null;
+                    }
+                    else if (value == "DONE")
+                    {
+                        goto Done;
+                    }
+                    else if (value != null)
+                    {
+                        
+                        Console.WriteLine(i % temp.Properties().Keys.Count);
+                        values[i % temp.Properties().Keys.Count] = value;
+                        
+                    }
+                    i++;
+                }
+               
+            }
+            Done:
+            var final = temp as ListInnitializable;
+            final.SetValuesWithList(values, bitflix);
+            return final;
         }
         private static void AddObjectToList(string[] args, Bitflix bitflix)
         {
